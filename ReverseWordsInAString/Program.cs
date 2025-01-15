0bff648ee3421639734827c2db265f88e71e1a24
KolesovDI <kolesovdi@it.mos.ru>
2025-01-15 20:34:36 +0300

    Init. Reverse Words in a string

---
+++ ReverseWordsInAString/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetcodeMedium
+{
+    /*
+     Given an input string s, reverse the order of the words.
+     A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+     Return a string of the words in reverse order concatenated by a single space.
+     Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = "Hello, World!";
+            Console.WriteLine(str);
+            Console.WriteLine(ReverseWords(str));
+            Console.ReadLine();
+        }
+
+        public static string ReverseWords(string s)
+        {
+            // Разделение строк на слова
+            string[] words = s.Split(' ');
+
+            // Создание новой строки для результата
+            string result = "";
+
+            // Проходим по словам в обратном порядке
+            for (int i = words.Length - 1; i >= 0; i--)
+            {
+                // Пропускаем пустые слова
+                if (!string.IsNullOrEmpty(words[i]))
+                {
+                    result += words[i].Trim() + " ";
+                }
+            }
+
+            // Удаляем лишний пробел в конце строки
+            return result.TrimEnd();
+        }
+    }
+}
