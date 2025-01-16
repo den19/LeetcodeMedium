fdc28f5d5d6233de5670e3ea8a742a935ee781ae
KolesovDI <kolesovdi@it.mos.ru>
2025-01-16 07:51:56 +0300

    Product of array except self

--- ProductOfArrayExceptSelf/Program.cs
+++ ProductOfArrayExceptSelf/Program.cs
@@ -9,7 +9,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] nums = { 1, 2, 3, 4 };
+            Console.WriteLine("Input array is:");
+            Array.ForEach(nums, (x) => Console.WriteLine(x));
+            var result = ProductExceptSelf(nums);
+            Console.WriteLine("Result is");
+            Array.ForEach(result, (x) => Console.WriteLine(x));// Output: 24, 12, 8, 6
+            Console.ReadLine();
+        }
+
+        public static int[] ProductExceptSelf(int[] nums)
+        {
+            int n = nums.Length;
+            int[] leftProducts = new int[n];
+            int[] rightProducts = new int[n];
+            int[] result = new int[n];
+
+            // Заполнение массива префиксных произведений
+            leftProducts[0] = 1;
+            for (int i = 1; i < n; i++)
+            {
+                leftProducts[i] = leftProducts[i - 1] * nums[i - 1];
+            }
+
+            // Заполнение массива постфиксных произведений
+            rightProducts[n - 1] = 1;
+            for (int i = n - 2; i >= 0; i--)
+            {
+                rightProducts[i] = rightProducts[i + 1] * nums[i + 1];
+            }
+
+            // Перемножение соответствующих элементов
+            for (int i = 0; i < n; i++)
+            {
+                result[i] = leftProducts[i] * rightProducts[i];
+            }
+
+            return result;
         }
     }
 }
